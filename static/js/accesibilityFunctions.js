339822b7659067de31a7a848b015a7bc50182407
Brian lml <brian.arias@unillanos.edu.co>
2023-01-24 21:28:13 -0500

    Botón Accesibilidad página main

---
+++ static/js/accesibilityFunctions.js
@@ -0,0 +1,7 @@
+let accessibilityBtn = document.getElementById("accesibilidadBtn");
+let accessibilityMenu = document.getElementById("accesibilidadMenu");
+
+accessibilityBtn.addEventListener("click", function() {
+    accessibilityMenu.classList.toggle("d-none");
+    accessibilityMenu.classList.toggle("d-block");
+  });
